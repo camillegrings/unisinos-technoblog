7909ca0e1827318b66c83552d721fb0dc4856695
camillegrings <camille.silva@cwi.com.br>
2020-04-02 20:58:44 -0300

    feat: exercicio 6

---
+++ aula4/exercicio6.js
@@ -0,0 +1,23 @@
+var quantidade = "25";
+var numero = 6;
+var pressao;
+var temperatura = 40;
+temperatura = null;
+
+console.log((quantidade += quantidade));
+// Resultado = 2525
+
+console.log((7 + 5) / numero + 2);
+// Resultado = 4
+
+console.log(pressao);
+// Resultado = undefined
+
+console.log(temperatura);
+// Resultado = null
+
+console.log(typeof pressao);
+// Resultado = undefined
+
+console.log(typeof temperatura);
+// Resultado = object
