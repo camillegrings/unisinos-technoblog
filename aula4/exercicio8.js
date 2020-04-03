4e1411ab32f804f447753522792afd1095ec3fdd
Luis Alberto Zagonel Pozenato <luis.pozenato@cwi.com.br>
2020-04-03 09:12:49 -0300

    Exercicio 8

---
+++ aula4/exercicio8.js
@@ -0,0 +1,19 @@
+var idade = 65;
+
+console.log(idade != 65);
+// Resultado = false
+
+console.log(idade >= 65);
+// Resultado = true
+
+console.log(idade == "65");
+// Resultado = true
+
+console.log(65 !== idade);
+// Resultado = false
+
+console.log(typeof (idade >  60));
+// Resultado = boolean
+
+console.log(typeof (idade >  70));
+// Resultado = boolean
