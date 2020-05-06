8b4c85dc05c6b7300a065702e68cc471e2758ad4
Luis Alberto Zagonel Pozenato <luis.pozenato@cwi.com.br>
2020-05-06 17:21:54 -0300

    displayCourses preference

--- trabalhoGA/javascript/preferences/index.js
+++ trabalhoGA/javascript/preferences/index.js
@@ -1,5 +1,6 @@
 var DEFAULT_FONT_SIZE = 14;
 var DEFAULT_READ_COLOR = "000000";
+var DEFAULT_DISPLAY_COURSES = "list";
 
 $(document).ready(function () {
   $(".fontSizeRadio").on("change", function () {
@@ -10,11 +11,18 @@ $(document).ready(function () {
     saveFontSize(size);
   });
 
-  $(".btn-color").on("click", function (val) {
+  $(".btn-color").on("click", function () {
     var color = this.id;
     saveReadColor(color);
     $(".btn-color").removeClass("active");
     $("#" + color).addClass("active");
+  });
+
+  $(".btn-display-courses").on("click", function () {
+    var displayCourses = this.id;
+    saveDisplayCourses(displayCourses);
+    $(".btn-display-courses").removeClass("active");
+    $("#" + displayCourses).addClass("active");
   });
 
   $("#reset-to-defaults").on("click", function () {
@@ -23,6 +31,7 @@ $(document).ready(function () {
 
   initFontSize();
   initReadColor();
+  initDisplayCourses();
 });
 
 function setFontSize(fontSize) {
@@ -36,14 +45,24 @@ function setReadColor(readColor) {
   $("#" + readColor).focus();
 }
 
-function saveReadColor(id) {
-  var color = DEFAULT_READ_COLOR;
-  localStorage.setItem("readcolor", id);
+function setDisplayCourses(displayCourse) {
+  $(".btn-display-courses").removeClass("active");
+  $("#" + displayCourse).addClass("active");
+  $("#" + displayCourse).focus();
+}
+
+function saveReadColor(color) {
+  localStorage.setItem("readcolor", color);
 }
 
 function saveFontSize(size) {
   localStorage.setItem("fontsize", size);
 }
+
+function saveDisplayCourses(display) {
+  localStorage.setItem("displayCourses", display);
+}
+
 
 function initFontSize() {
   var fontSize = localStorage.getItem("fontsize");
@@ -59,6 +78,14 @@ function initReadColor() {
     readColor = DEFAULT_READ_COLOR;
   }
   setReadColor(readColor);
+}
+
+function initDisplayCourses() {
+  var displayCourses = localStorage.getItem("displayCourses");
+  if (displayCourses === null) {
+    displayCourses = DEFAULT_DISPLAY_COURSES;
+  }
+  setDisplayCourses(displayCourses);
 }
 
 function getReadColor() {
@@ -77,9 +104,19 @@ function getFontSize() {
   return fontSize;
 }
 
+function getDisplayCourses() {
+  var displayCourses = localStorage.getItem("displayCourses");
+  if (displayCourses === null) {
+    displayCourses = DEFAULT_DISPLAY_COURSES;
+  }
+  return displayCourses;
+}
+
 function resetToDefaults() {
   setFontSize(DEFAULT_FONT_SIZE);
   saveFontSize(DEFAULT_FONT_SIZE);
   setReadColor(DEFAULT_READ_COLOR);
   saveReadColor(DEFAULT_READ_COLOR);
+  setDisplayCourses(DEFAULT_DISPLAY_COURSES);
+  saveDisplayCourses(DEFAULT_DISPLAY_COURSES);
 }
