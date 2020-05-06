08b84f794f4dcb8d4b2518ca323cee0a20ffc95d
Camille Silva <camille.silva@cwi.com.br>
2020-05-05 21:17:37 -0300

    focus botao cor

--- trabalhoGA/javascript/preferences/index.js
+++ trabalhoGA/javascript/preferences/index.js
@@ -1,76 +1,77 @@
 var DEFAULT_FONT_SIZE = 14;
-var DEFAULT_READ_COLOR = '17a2b8';
+var DEFAULT_READ_COLOR = "17a2b8";
 
 $(document).ready(function () {
-
-  $('.fontSizeRadio').on('change', function() {
-    var size = $('input[name=fontSizeRadio]:checked', '#preferences-form').val(); 
+  $(".fontSizeRadio").on("change", function () {
+    var size = $(
+      "input[name=fontSizeRadio]:checked",
+      "#preferences-form"
+    ).val();
     saveFontSize(size);
   });
 
-  $('.btn-color').on('click', function(val) {
+  $(".btn-color").on("click", function (val) {
     var color = this.id;
     saveReadColor(color);
-    $('.btn-color').removeClass('active');
-    $('#'+color).addClass('active');
+    $(".btn-color").removeClass("active");
+    $("#" + color).addClass("active");
   });
 
-  $('#reset-to-defaults').on('click', function() {
+  $("#reset-to-defaults").on("click", function () {
     resetToDefaults();
   });
 
   initFontSize();
   initReadColor();
-
- });
-
+});
 
 function setFontSize(fontSize) {
-  var $radios = $('input:radio[name=fontSizeRadio]');
-  $radios.filter('[value='+fontSize.toString()+']').prop('checked', true);
+  var $radios = $("input:radio[name=fontSizeRadio]");
+  $radios.filter("[value=" + fontSize.toString() + "]").prop("checked", true);
 }
 
 function setReadColor(readColor) {
-  $('.btn-color').removeClass('active');
-  $('#'+readColor).addClass('active');
+  $(".btn-color").removeClass("active");
+  $("#" + readColor).addClass("active");
+  $("#" + readColor).focus();
 }
 
-function saveReadColor(id){
+function saveReadColor(id) {
   var color = DEFAULT_READ_COLOR;
-  localStorage.setItem('readcolor', id);
+  localStorage.setItem("readcolor", id);
 }
 
-function saveFontSize(size){
-  localStorage.setItem('fontsize', size);
+function saveFontSize(size) {
+  localStorage.setItem("fontsize", size);
 }
 
-function initFontSize(){
-  var fontSize = localStorage.getItem('fontsize');
-  if(fontSize === null) {
+function initFontSize() {
+  var fontSize = localStorage.getItem("fontsize");
+  if (fontSize === null) {
     fontSize = DEFAULT_FONT_SIZE;
   }
   setFontSize(fontSize);
 }
 
-function initReadColor(){
-  var readColor = localStorage.getItem('readcolor');
-  if(readColor === null) {
+function initReadColor() {
+  var readColor = localStorage.getItem("readcolor");
+  if (readColor === null) {
     readColor = DEFAULT_READ_COLOR;
   }
   setReadColor(readColor);
 }
 
 function getReadColor() {
-  var readColor = localStorage.getItem('readcolor');
-  if(readColor === null) {
+  var readColor = localStorage.getItem("readcolor");
+  if (readColor === null) {
     readColor = DEFAULT_READ_COLOR;
   }
   return readColor;
 }
 
 function getFontSize() {
-  var fontSize = localStorage.getItem('fontsize');
-  if(fontSize === null) {
+  var fontSize = localStorage.getItem("fontsize");
+  if (fontSize === null) {
     fontSize = DEFAULT_FONT_SIZE;
   }
   return fontSize;
