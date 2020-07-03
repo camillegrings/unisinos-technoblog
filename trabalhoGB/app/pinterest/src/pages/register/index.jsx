ce0d4592a42df30d882425ed212e63fadf32a7dc
Julio-Saldanha <62161520+Julio-Saldanha@users.noreply.github.com>
2020-07-02 21:34:45 -0300

    Register

--- trabalhoGB/app/pinterest/src/pages/register/index.jsx
+++ trabalhoGB/app/pinterest/src/pages/register/index.jsx
@@ -1,14 +1,45 @@
 import React from "react";
-// import "./App.css";
+import "./style.css";
 
-function Register() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Register</p>
-      </header>
-    </div>
-  );
-}
+import { Header } from "../../components";
+
+
+
+function Register(){
+ function renderElements(){
+ return(  
+   React.createElement(
+    "textarea",
+    {id :"Nome"},
+    "Adicione um titulo"
+  ),
+
+  React.createElement(
+    "textarea",
+    {id :"Nome"},
+    "Conte sobre a sua criação"
+  ),
+
+ React.createElement(
+    "input",
+    {id : "upload"},
+    "cole aqui seu trabalho"
+  )  
+ );
+ }
+  
+
+
+ return (
+  <div>
+    <header className="App-header">
+      <Header />
+    </header>
+    <div className="container">{renderElements()}</div>
+  </div>
+);
+
+};
+
 
 export default Register;
