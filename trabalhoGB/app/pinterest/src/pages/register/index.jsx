e93d6d7618cdd976268e22afca26bc7a5122bf1e
Julio-Saldanha <62161520+Julio-Saldanha@users.noreply.github.com>
2020-07-03 17:56:16 -0300

    css
    Não consegui finalizar o css da tela de casdastro de imagens

--- trabalhoGB/app/pinterest/src/pages/register/index.jsx
+++ trabalhoGB/app/pinterest/src/pages/register/index.jsx
@@ -19,13 +19,15 @@ function Register() {
     return images.map((image) => {
       return (     
 
-      <div class="container" id="body">
+      <div id="body" class="container">
           
+          <form id="elementos">
+
        <div id="uploadDiv"><input id="upload" type="file" accept="image/bmp,image/gif,image/jpeg,image/png,image/tiff,image/webp" ></input></div>
         <div id="nomeDiv"><input type="text" placeholder="Adicione um titulo" name="Nome" id="Nome"/></div>
-        
          <div id="descricaoDiv"><input type="text" placeholder="Conte sobre o que é seu PIN" name="Descricao" id="Descricao"/></div>
-        
+
+          </form>
         </div>
           
       );
